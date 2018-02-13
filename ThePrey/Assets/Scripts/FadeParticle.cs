578958d1190fbcb9799f8c6a53653ba565875b98
Axel 'Loess' Lavielle <lavielleaxel@gmail.com>
2018-02-12 19:18:09 -0500

    Adding the "smell" of the NPC as particle effect, need a bit of hyperparameter tuning, also have done basic movements that will be deleted later but allows us to test

---
+++ ThePrey/Assets/Scripts/FadeParticle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeParticle : MonoBehaviour {
+	ParticleSystemRenderer p;
+	int numParticlesAlive;
+	ParticleSystem m_System;
+	ParticleSystem.Particle[] m_Particles;
+
+	void InitializeIfNeeded()
+	{
+		if (m_System == null)
+			m_System = GetComponent<ParticleSystem>();
+		if (m_Particles == null || m_Particles.Length < m_System.main.maxParticles)
+			m_Particles = new ParticleSystem.Particle[m_System.main.maxParticles];
+	}
+
+	private void LateUpdate()
+	{
+		InitializeIfNeeded();
+
+		// GetParticles is allocation free because we reuse the m_Particles buffer between updates
+		int numParticlesAlive = m_System.GetParticles(m_Particles);
+
+		// Change only the particles that are alive
+		for (int i = 0; i < numParticlesAlive; i++)
+		{
+		}
+
+		// Apply the particle changes to the particle system
+		m_System.SetParticles(m_Particles, numParticlesAlive);
+	}
+
+}
---
+++ ThePrey/Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f, 0);
+		transform.Translate(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * 3.0f);
+	}
+}
