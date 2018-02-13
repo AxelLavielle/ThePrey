4361b210d8b445baa7c9d3ea8a85a17cc0eeb767
Axel 'Loess' Lavielle <lavielleaxel@gmail.com>
2018-02-12 20:48:29 -0500

    Player movements with mouse + keyboard finished, just did not checked the mouse cursor for zoom in/out since I don't have a mouse

---
+++ ThePrey/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	[SerializeField]
+	GameObject target;
+	[SerializeField]
+	float distToPlayer;
+	[SerializeField]
+	float rotation;
+	[SerializeField]
+	float rotSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distToPlayer += Input.GetAxis ("Mouse ScrollWheel");
+		if (distToPlayer < 1.5f)
+			distToPlayer = 1.5f;
+		else if (distToPlayer > 10)
+			distToPlayer = 10;
+		rotation += Input.GetAxis ("Mouse Y") / rotSpeed;
+		if (rotation <= 0)
+			rotation = 0.001f;
+		else if (rotation >= 1)
+			rotation = 0.999f;
+		Vector3 offset = target.transform.forward * -1 * rotation + target.transform.up * (1 - rotation);
+		offset.Normalize ();
+		transform.position = target.transform.position + offset * distToPlayer;
+		transform.LookAt (target.transform);
+	}
+}
---
+++ ThePrey/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	// public vars
+	public float walkSpeed = 6;
+	public float rotateSpeed = 3;
+
+	// System vars
+	Vector3 moveAmount;
+	Vector3 smoothMoveVelocity;
+	Rigidbody rigidbody;
+
+
+	void Awake() {
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+		rigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void Update() {
+
+		// Calculate movement:
+		float inputX = Input.GetAxisRaw("Horizontal");
+		float inputY = Input.GetAxisRaw("Vertical");
+        float mouseX = Input.GetAxis("Mouse X");
+
+        Vector3 moveDir = new Vector3(inputX, 0, inputY).normalized;
+		Vector3 targetMoveAmount = moveDir * walkSpeed;
+		moveAmount = Vector3.SmoothDamp(moveAmount,targetMoveAmount,ref smoothMoveVelocity,.15f);
+		transform.Rotate(Vector3.up * mouseX * rotateSpeed);
+
+	}
+
+	void FixedUpdate() {
+		// Apply movement to rigidbody
+		Vector3 localMove = transform.TransformDirection(moveAmount) * Time.fixedDeltaTime;
+		rigidbody.MovePosition(rigidbody.position + localMove);
+	}
+}
