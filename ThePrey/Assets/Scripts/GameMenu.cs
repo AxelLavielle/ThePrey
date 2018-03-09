d6cdca1ead0683858bd842cd1f9a7d4206d9ae74
Unknown <alice.pesty@epitech.eu>
2018-03-09 18:22:04 -0500

    Pause menu on escape resume on escape

---
+++ ThePrey/Assets/Scripts/GameMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenu : MonoBehaviour {
+    public GameObject menuPause;
+    public GameObject game;
+    public GameObject mainCam;
+
+	// Use this for initialization
+	void Start () {
+        print("make pause menu inactive");
+        mainCam = GameObject.Find("Main Camera");
+        //menuPause.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (menuPause.activeSelf)
+            {
+                resumeGame();
+                return;
+            }
+            menuPause.SetActive(true);
+            mainCam.GetComponent<CameraFollow>().enabled = false;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            game.SetActive(false);
+        }
+    }
+
+    public void resumeGame()
+    {
+        menuPause.SetActive(false);
+        mainCam.GetComponent<CameraFollow>().enabled = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        game.SetActive(true);
+    }
+}
