d5217a69def06f440733f4f0cdafc24ddf6d1b5c
alicepesty <alice.pesty@epitech.eu>
2018-03-09 19:41:18 -0500

    End scene

---
+++ ThePrey/Assets/Scripts/Ending.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ending : MonoBehaviour {
+    public Text endingMessage;
+
+
+	// Use this for initialization
+	void Start () {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        if (EndGame.victory)
+            endingMessage.text = "VICTORY !";
+        else
+            endingMessage.text = "Defeat ...";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
