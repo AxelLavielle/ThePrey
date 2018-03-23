6d9fb1c8e2ef08b01fefa0390d9333de22eacbc2
alicepesty <alice.pesty@epitech.eu>
2018-03-23 18:06:12 -0400

    minimap light fixed

---
+++ ThePrey/Assets/Scripts/Minimap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour {
+
+//    public List<Light> lights;
+
+    private void OnPreCull()
+    {
+        RenderSettings.ambientLight = Color.white;
+    }
+
+    private void OnPostRender()
+    {
+        RenderSettings.ambientLight = Color.black;
+    }
+}
